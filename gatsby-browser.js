5c4e4e251a6e4eb3184c44fab3ea7668f3324308
smithpg <blu2penguin@gmail.com>
2019-01-05 23:34:02 -0800

    implemented shouldUpdateScroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppProvider from 'store/provider';
 import wrapPageElementWithTransition from 'helpers/wrapPageElement';
+import { timeout } from './src/constants/transition';
 
 // React Context in Browser
 // eslint-disable-next-line react/prop-types
@@ -10,3 +11,20 @@ export const wrapRootElement = ({ element }) => {
 
 // Page Transitions
 export const wrapPageElement = wrapPageElementWithTransition;
+
+// Prevent unsightly scroll to top on exit transitions
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0), timeout);
+  } else {
+    const savedPosition = getSavedScrollPosition(location);
+    window.setTimeout(
+      () => window.scrollTo(...(savedPosition || [0, 0])),
+      timeout
+    );
+  }
+  return false;
+};
